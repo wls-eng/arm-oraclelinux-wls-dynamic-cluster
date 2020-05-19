ae6e264de0026688063bf7a56091596259007597
Galia <haixia.cheng@mircosoft.com>
2020-05-19 10:58:51 +0800

    Update weblogic-deploy-tooling version to 1.8.1, to fix error: "You are using an unsupported JDK version 11.0.7+8-LTS".

--- arm-oraclelinux-wls-dynamic-cluster/src/main/scripts/setupDynamicClusterDomain.sh
+++ arm-oraclelinux-wls-dynamic-cluster/src/main/scripts/setupDynamicClusterDomain.sh
@@ -498,7 +498,7 @@ export nmHost=`hostname`
 export nmPort=5556
 export machineNamePrefix="machine"
 export machineName="$machineNamePrefix-$nmHost"
-export WEBLOGIC_DEPLOY_TOOL=https://github.com/oracle/weblogic-deploy-tooling/releases/download/weblogic-deploy-tooling-1.1.1/weblogic-deploy.zip
+export WEBLOGIC_DEPLOY_TOOL=https://github.com/oracle/weblogic-deploy-tooling/releases/download/weblogic-deploy-tooling-1.8.1/weblogic-deploy.zip
 export username="oracle"
 export groupname="oracle"
 
