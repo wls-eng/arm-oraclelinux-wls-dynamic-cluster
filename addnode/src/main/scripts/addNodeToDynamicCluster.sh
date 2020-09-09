234dd93cd318b6b0772dcc8cc1f2f4cd941256df
Galia <haixia.cheng@microsoft.com>
2020-09-09 14:00:57 +0800

    Upgrade deploy tool to 1.8.1 to support JDK 11.

--- addnode/src/main/scripts/addNodeToDynamicCluster.sh
+++ addnode/src/main/scripts/addNodeToDynamicCluster.sh
@@ -557,7 +557,7 @@ export wlsClusterName="cluster1"
 export dynamicServerTemplate="myServerTemplate"
 export machineNamePrefix="machine"
 export machineName="$machineNamePrefix-$nmHost"
-export WEBLOGIC_DEPLOY_TOOL=https://github.com/oracle/weblogic-deploy-tooling/releases/download/weblogic-deploy-tooling-1.1.1/weblogic-deploy.zip
+export WEBLOGIC_DEPLOY_TOOL=https://github.com/oracle/weblogic-deploy-tooling/releases/download/weblogic-deploy-tooling-1.8.1/weblogic-deploy.zip
 export username="oracle"
 export groupname="oracle"
 
