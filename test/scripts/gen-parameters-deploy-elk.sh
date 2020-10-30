e5ed9480a94c1ce60c11e98d18f12986e087340c
haixia <haixia.cheng@microsoft.com>
2020-10-30 16:11:01 +0800

    ELK on coherence template
    Replace hard code ARM API with ${azure.apiVersion2}

--- test/scripts/gen-parameters-deploy-elk.sh
+++ test/scripts/gen-parameters-deploy-elk.sh
@@ -23,7 +23,7 @@ cat <<EOF > ${parametersPath}
         "value": "${adminVMName}"
       },
       "elasticsearchPassword": {
-        "value": "elasticsearchPassword"
+        "value": "${elasticsearchPassword}"
       },
       "elasticsearchEndpoint": {
         "value": "${elasticsearchURI}"
@@ -52,7 +52,7 @@ cat <<EOF > ${parametersPath}
       "maxDynamicClusterSize": {
         "value": ${maxDynamicClusterSize}
       },
-      "dynamicClusterSize": {
+      "numberOfManagedApplicationInstances": {
         "value": ${dynamicClusterSize}
       }
     }
